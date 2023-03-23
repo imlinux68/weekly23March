5d5f73e311084478479dcb5a3f115ac45ff92a40
im linux <imlinux68@gmail.com>
2023-03-23 15:28:38 +0200

    updated wit for loop

--- script2.sh
+++ script2.sh
@@ -1,9 +1,25 @@
- #!/bin/bash
-function makefiles () {
-    for i in "$@"
-        do 
-            touch "$i"
-        done
-}
+#!/bin/bash
+
+#function makefiles () {
+#	if [[ $# -lt 5 ]]
+#		then 
+#			echo "error"
+#		else
+#   		for i in "$@"
+#        	do 
+#            	touch "$i"
+#       		 done
+#	fi
+#}
 
 #makefiles $@
+
+
+function makefiles2 () {
+	for (( i=1; i<5; i++ ))
+		do
+			read -rp "Enter $i file name: " fname
+			touch $fname
+		done
+}
+makefiles2
